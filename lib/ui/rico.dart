af501cb252585f4af90351b6a696fe3e8e0e1de4
Raul <raul.sjbv@gmail.com>
2019-06-27 16:58:08 -0300

    Alteracao valor inicial da variavel contador

--- lib/ui/rico.dart
+++ lib/ui/rico.dart
@@ -11,7 +11,7 @@ class Rico extends StatefulWidget {
 //classe interna do tipo State, que recebe a Class Rico
 class _RicoState extends State<Rico> {
 
-  double _contador = 10;
+  double _contador = 6;
 
   void_showDialog()
   {
